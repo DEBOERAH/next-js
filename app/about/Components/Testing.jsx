750700b73ea5b1a526e8ee1dc3b9fb759bec4358
DEBOERAH <eniolaofuani@gmail.com>
2024-12-22 18:48:27 +0100

    initial commit

---
+++ app/about/Components/Testing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique quis doloribus omnis illo quo facilis incidunt consequuntur dolor, aliquid quisquam quas dicta. Eos ratione qui nam optio nemo minus ab saepe quo labore blanditiis numquam dolorum, doloremque recusandae sapiente commodi quia ad alias eius, nisi rerum? Error quis veniam sint pariatur, reiciendis sed deleniti est tenetur non obcaecati nam quae explicabo aliquid maxime perferendis quas dolorem ex nobis quos at ab beatae consequuntur. Quod eius voluptas inventore, earum quo id?</div>
+  )
+}
+
+export default Testing
---
+++ app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Testing from './Components/Testing'
+import Page from './Ife/Page'
+
+const page = () => {
+  return (
+    <div>
+        <h1>I am testing</h1>
+        <Page/>
+        <Testing/>
+    </div>
+
+  )
+}
+
+export default page
